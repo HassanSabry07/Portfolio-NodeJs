967be8bc8f8921bc66664b41c699ab821e03008d
HassanSabry07 <hsabry28@yahoo.com>
2026-03-06 16:12:14 +0200

    fix auth

--- user.js
+++ user.js
@@ -1,21 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-userSchema.pre('save', async function() {
-  if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 10);
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
