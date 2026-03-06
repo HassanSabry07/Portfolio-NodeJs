d299d934bf4893006fae3954c0b87d8a389ab058
HassanSabry07 <hsabry28@yahoo.com>
2026-03-06 16:02:56 +0200

    fix password hashing

--- user.js
+++ user.js
@@ -13,9 +13,10 @@ const userSchema = mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function() {
-  if (!this.isModified('password')) return;
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
